aaeb301c7bf5f8fbe80cfe77b2b4b5d4aa58167f
clara-jr <clarajimenezrecio@gmail.com>
2024-03-28 00:12:26 +0100

    feat: jsdoc

--- src/services/service.ts
+++ src/services/service.ts
@@ -1,9 +1,18 @@
 import { Model, ModelType } from '../models/model.ts';
 
+/**
+ * Retrieves all items from the database.
+ * @returns {Promise<Array>} A promise that resolves to an array of items.
+ */
 async function getAll() {
   return Model.find();
 }
 
+/**
+ * Creates a new item in the database.
+ * @param {*} data The data for the new item.
+ * @returns {Promise<Object>} A promise that resolves to the created item.
+ */
 async function create(data: ModelType) {
   return Model.create(data);
 }
