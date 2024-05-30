1162db3d286b2f7156b405f87f0e26cc491722fd
clara-jr <clarajimenezrecio@gmail.com>
2024-05-30 14:35:36 +0200

    feat: use mongodb-memory-server in tests

--- test/_setup.ts
+++ test/_setup.ts
@@ -1,9 +1,15 @@
 import server from '../src/server.ts';
 import { styleText } from 'node:util';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+
+let mongoServer;
 
 before(async () => {
   console.info(styleText('bgWhiteBright', '🤖 Launching server before running tests...'));
   try {
+    // Start MongoDB in-memory server
+    mongoServer = await MongoMemoryServer.create();
+    process.env.MONGODB_URI = mongoServer.getUri();
     await server.start();
   } catch (error) {
     console.error(styleText('bgRedBright', error));
@@ -14,6 +20,10 @@ after(async () => {
   console.info(styleText('bgWhiteBright', '🤖 Stopping server after running tests...'));
   try {
     await server.stop();
+    // Stop MongoDB in-memory server
+    if (mongoServer) {
+      await mongoServer.stop();
+    }
   } catch (error) {
     console.error(styleText('bgRedBright', error));
   }
